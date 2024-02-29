03308c4f23d7e6f54e4993c02410da645c4e9d71
uditsinghs <153190852+uditsinghs@users.noreply.github.com>
2024-02-29 08:04:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+// let age = 21;
+//  function printAge(){
+//   console.log(`The age is ${age}`);
+
+// }
+// printAge();
+
+//  age = 45;
+// printAge();
+
+
+
+// function driveOrNot(){
+//   let age =17;
+//   let status;
+//   if(age>=18){
+// status='you can drive';
+//   }
+//   else{
+//     status= 'you can not drive';
+//   }
+//   return status;
+// }
+// console.log(driveOrNot());
+
+// To gnerate random number between 0 to 3 
+
+// function generateRandom(){
+//   let random = Math.random();
+//   let randomMax = random*3;
+//   return randomMax;
+// }
+// console.log(generateRandom());
+
+// solve exercise
+
+// Number is even odd
+
+// function isOdd(num){
+//   let rem = num%2;
+//   let isodd = rem == 1;
+//   return isodd;
+// }
+// console.log(isOdd(5));
+
+
+// largest of two number
+
+// function largest(num1,num2){
+//   if(num1>num2){
+//     console.log(`the greatest of Two number is ${num1}`);
+//   }
+//   else{
+//     console.log(`the greatest of Two number is ${num2}`);
+//   }
+// }
+// largest(8,5);
+
+// using ternary operator
+
+// function larger(num1,num2){
+//   return num1>num2 ? num1 : num2 
+// }
+// console.log(larger(5,6));
+
+
+
